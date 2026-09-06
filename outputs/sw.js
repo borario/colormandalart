a8e59cf0bf5161db6a51a161cc8d627b339eee05
borario <borahwang2@gmail.com>
2026-09-07 00:38:28 +0900

    Publish guide version with trace branding

---
+++ outputs/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "color-mandalart-v8";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./site.webmanifest",
+  "./assets/favicon.png",
+  "./assets/apple-touch-icon.png",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png",
+  "./assets/icon-close.png",
+  "./assets/icon-copy.png",
+  "./assets/icon-download.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html")))
+  );
+});
